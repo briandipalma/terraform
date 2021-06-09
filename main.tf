a967b9fda8e17d723a1801a04023646550c7b906
Brian Di Palma <briandipalma [at] outlook.com>
2021-06-09 21:40:37 +0100

    feat: Add proxmox provider config and init lock

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "telmate/proxmox"
+      version = "2.7.1"
+    }
+  }
+}
+
 provider "proxmox" {
     pm_api_url = "https://pve.test:8006/api2/json"
 }
