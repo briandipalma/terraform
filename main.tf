f17cabb1c2d46c5b71a838367519636965c06a65
Brian Di Palma <briandipalma [at] outlook.com>
2021-06-13 22:48:32 +0100

    feat: Add a mountpoint

--- main.tf
+++ main.tf
@@ -19,11 +19,25 @@ resource "proxmox_lxc" "basic" {
   ostemplate   = "local:vztmpl/ubuntu-21.04-standard_21.04-1_amd64.tar.gz"
   password     = "BasicLXCContainer"
   unprivileged = true
+  ostype = "ubuntu"
 
   // Terraform will crash without rootfs defined
   rootfs {
     storage = "local-lvm"
     size    = "8G"
+  }
+
+  mountpoint {
+    mp      = "/mnt/container/bind-mount-point"
+    size    = "8G"
+    slot    = 0
+    key     = "0"
+    storage = "/srv/test"
+    // Without 'volume' defined, Proxmox will try to create a volume with
+    // the value of 'storage' + : + 'size' (without the trailing G) - e.g.
+    // "/srv/host/bind-mount-point:256".
+    // This behaviour looks to be caused by a bug in the provider.
+    volume  = "/srv/test"
   }
 
   network {
