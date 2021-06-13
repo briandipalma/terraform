4e9fb7bf0559eb62d5421e58d517f81967c95e1d
Brian Di Palma <briandipalma [at] outlook.com>
2021-06-13 12:49:50 +0100

    feat: Create a basic LXC container

--- main.tf
+++ main.tf
@@ -8,5 +8,27 @@ terraform {
 }
 
 provider "proxmox" {
-    pm_api_url = "https://pve.test:8006/api2/json"
+  pm_api_url = var.proxmox_api_url
+  // Required when using self signed certs
+  pm_tls_insecure = true
+}
+
+resource "proxmox_lxc" "basic" {
+  target_node  = "pve"
+  hostname     = "lxc-basic"
+  ostemplate   = "local:vztmpl/ubuntu-21.04-standard_21.04-1_amd64.tar.gz"
+  password     = "BasicLXCContainer"
+  unprivileged = true
+
+  // Terraform will crash without rootfs defined
+  rootfs {
+    storage = "local-lvm"
+    size    = "8G"
+  }
+
+  network {
+    name   = "eth0"
+    bridge = "vmbr0"
+    ip     = "dhcp"
+  }
 }
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "proxmox_api_url" {
+  default = "https://pve.test:8006/api2/json"
+  type = string
+  description = "The Proxmox API URL"
+}
