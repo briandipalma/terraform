72a37d4b05ea018b0b01810178fcb9e6b571ff49
Brian Di Palma <briandipalma [at] outlook.com>
2021-06-18 07:13:13 +0100

    refactor: Extract required providers to versions.tf file

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    proxmox = {
-      source = "telmate/proxmox"
-      version = "2.7.1"
-    }
-  }
-}
-
 provider "proxmox" {
   pm_api_url = var.proxmox_api_url
   pm_user = var.proxmox_user
